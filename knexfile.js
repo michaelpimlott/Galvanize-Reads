3062e22fd42b47657db035504ec70420d74f6ff5
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-04-18 20:32:31 -0700

    gone swimming

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,17 @@
 require('dotenv').load();
 
 module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/galvanize-reads'
+  },
 
-   development: {
-     client: 'pg',
-     connection: 'postgres://localhost/galvanize-reads'
-   },
-
-   production: {
-     client: 'pg',
-     connection: process.env.DATABASE_URL + '?ssl=true'
-   }
-
- };
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    pool: {
+      min: 0,
+      max: 7
+    }
+  }
+};
